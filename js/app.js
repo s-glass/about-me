43ef5c58939842f28640e0125d2ab93ae7910863
SARAH GLASS <SGLASS87@GMAIL.COM>
2023-02-07 12:14:00 -0800

    adds initial structure for lab

---
+++ js/app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+console.log('hey world');
+
+// TODO: Get users name - prompt and alert greet back to user
+
+let userName = prompt('What is your name?');
+
+alert(`Welcome to my site ${userName}! Lets play a guessing game...please answer yes or no`);
+
+
+// TODO: prompt 5 yes/no or y/n questions for the user to guess and alert when they are correct or incorrect
+
+let questionOneGuess = prompt('Do I have a dog?').toLowerCase();
+
+if(questionOneGuess === 'yes' || questionOneGuess === 'y'){
+  alert('You are correct!');
+} else if(questionOneGuess === 'no' || questionOneGuess === 'n'){
+  alert ('Sorry you are incorrect');
+}
+
+
+// TODO: give them a final message with their name in the alert 
+
+
+alert(`Thanks for playing ${userName}!`);
